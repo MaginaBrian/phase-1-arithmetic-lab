520a1fc547281b6797a7c88dae9686e751bb5884
MaginaBrian <brianojuk@gmail.com>
2025-03-03 22:46:41 +0300

    final changes

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+// Multiply two numbers to get 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;  
+
+// Generate random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;  
+
+// Calculate remainder to get 4
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;  
+
+// Find max number returning 20
+const max = Math.max(5, 10, 15, 20, 12); 
+
